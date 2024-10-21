d0ab972b912a9b094aa2313cde88b74aff10ee22
Innocentprogrammer <mratyunjaysaxena8859@gmail.com>
2024-10-21 21:46:28 +0530

    update

--- Fire Detector.py
+++ Fire Detector.py
@@ -1,7 +1,6 @@
 import cv2         
 import threading   
 import playsound   
-from twilio.rest import Client   
 
 # The below line is to access xml file which includes positive and negative images of fire. (Trained images) File is also provided within the quote "NO MR_STOCK FOOTAGE NO MR (1831)_preview.mp4" 
 fire_cascade = cv2.CascadeClassifier('fire_detection.xml') 
@@ -10,27 +9,11 @@ video = cv2.VideoCapture(0)
 runOnce = False 
 Message_Status = False
 
-# Twilio credentials (replace with your own)
-# account_sid = 'AC544f52444dd4bedff81d1d8fa407a651'
-# auth_token = '3bda45e5908ff42f0f1e0966f0ce70ea'
-# twilio_phone_number = '+12035294362'
-# recipient_phone_number = '+918445518517'
 
 def play_alarm_sound_function(): 
     playsound.playsound('fire_Alarm.mp3',True) 
     print("Fire alarm end") 
 
-# def send_message_function():
-#     try:
-#         client = Client(account_sid, auth_token)
-#         message = client.messages.create(
-#             body="Warning! A Fire Accident has been detected.",
-#             from_=twilio_phone_number,
-#             to=recipient_phone_number
-#         )
-#         print(f"Message sent to {recipient_phone_number}")
-#     except Exception as e:
-#         print(e)
 		
 while(True):
     Alarm_Status = False
@@ -45,9 +28,7 @@ while(True):
 
         print("Fire alarm initiated")
         threading.Thread(target=play_alarm_sound_function).start() 
-        # if Message_Status == False:
-        #     threading.Thread(target=send_message_function).start()
-        #     Message_Status = True
+
 
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
